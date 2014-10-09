18fe11ddec899944178b2148de0e76bf2e22c447
slstevens <sean.stevens.123@gmail.com>
2014-10-09 17:55:40 +0100

    Added broken and break methods

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -5,4 +5,10 @@ describe Bike do
 		bike = Bike.new
 		expect(bike).not_to be_broken
 	end
+
+	it "should be able to break" do
+		bike = Bike.new
+		bike.break!
+		expect(bike).to be_broken
+	end
 end
